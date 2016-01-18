afdf0bbab104ffbb92e543f554fddb0641f132a7
Umashankar Subramanian <umashankar.s@live.com>
2016-01-17 17:36:45 -0800

    bug fixes and some clean up

--- sample.js
+++ sample.js
@@ -1,77 +1,84 @@
 'use strict';
 
-const SlackBot = require('slack-quick-bots');
+const SlackBot = require('./lib/index');
 const handlebars = require('handlebars');
 const fs = require('fs');
 const sampleTemplate = fs.readFileSync('./sample.hbs', 'utf8');
 
 var config = {
   'bots': [{
-  'botCommand': {
-    'PING': {
-      'commandType': 'DATA',
-      'allowedParam': ["hello", "status"],
-      'timeUnit': 'm',
-      'defaultParamValue': "hello",
-      'template': function() {
-        return handlebars.compile(sampleTemplate);
+    'botCommand': {
+      'PING': {
+        'commandType': 'DATA',
+        'allowedParam': [1, 2],
+        'defaultParamValue': 1,
+        'template': function() {
+          return handlebars.compile(sampleTemplate);
+        },
+        'data': function(command, param, callback) {
+          console.log(param);
+          callback({
+            'param': param
+          });
+        }
       },
-      'data': function(command, param, callback) {
-        callback({'param': param});
+      'AUTO': {
+        'commandType': 'RECURSIVE',
+        'lowerLimit': 0,
+        'upperLimit': 100,
+        'defaultParamValue': 1,
+        'template': function() {
+          return handlebars.compile(sampleTemplate);
+        },
+        'data': function(command, param, callback) {
+          callback({
+            'param': param
+          });
+        }
+      },
+      'STOP': {
+        'commandType': 'KILL',
+        'parentTask': 'AUTO'
       }
     },
-    'AUTO': {
-      'commandType': 'RECURSIVE',
-      'lowerLimit': 0,
-      'upperLimit': 100,
-      'defaultParamValue': 1,
-      'template': function() {
-        return handlebars.compile(sampleTemplate);
+    'botToken': ''
+  }, {
+    'botCommand': {
+      'STATUS': {
+        'commandType': 'DATA',
+        'allowedParam': ['what', 'there'],
+        'timeUnit': 'm',
+        'defaultParamValue': 'what',
+        'template': function() {
+          return handlebars.compile(sampleTemplate);
+        },
+        'data': function(command, param, callback) {
+          callback({
+            'param': param
+          });
+        }
       },
-      'data': function(command, param, callback) {
-        callback({'param': param});
+      'UPDATE': {
+        'commandType': 'RECURSIVE',
+        'lowerLimit': 0,
+        'upperLimit': 100,
+        'defaultParamValue': 1,
+        'template': function() {
+          return handlebars.compile(sampleTemplate);
+        },
+        'data': function(command, param, callback) {
+          callback({
+            'param': param
+          });
+        }
+      },
+      'STOP': {
+        'commandType': 'KILL',
+        'parentTask': 'UPDATE'
       }
     },
-    'STOP': {
-      'commandType': 'KILL',
-      'parentTask': 'AUTO'
-    }
-  },
-  'botToken': ''
-},
-{
-  'botCommand': {
-    'STATUS': {
-      'commandType': 'DATA',
-      'allowedParam': ["what", "there"],
-      'timeUnit': 'm',
-      'defaultParamValue': "what",
-      'template': function() {
-        return handlebars.compile(sampleTemplate);
-      },
-      'data': function(command, param, callback) {
-        callback({'param': param});
-      }
-    },
-    'UPDATE': {
-      'commandType': 'RECURSIVE',
-      'lowerLimit': 0,
-      'upperLimit': 100,
-      'defaultParamValue': 1,
-      'template': function() {
-        return handlebars.compile(sampleTemplate);
-      },
-      'data': function(command, param, callback) {
-        callback({'param': param});
-      }
-    },
-    'STOP': {
-      'commandType': 'KILL',
-      'parentTask': 'UPDATE'
-    }
-  },
-  'botToken': ''
-}]
+    'botToken': ''
+  }]
 };
 var slackBot = new SlackBot(config);
 slackBot.start();
