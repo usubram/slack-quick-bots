0ae3f6c66ae7081cad651f0c2cb52d864a7e2249
Umashankar Subramanian <umashankar.s@live.com>
2016-04-03 21:51:09 -0700

    update path

--- utils/environment.js
+++
@@ -1,9 +0,0 @@
-function getEnvironment() {
-  if (process.env.NODE_ENV === 'development') {
-    return {dev: true};
-  } else {
-    return {prod: true};
-  }
-};
-
-exports = module.exports = getEnvironment();
