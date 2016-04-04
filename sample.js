b7c3ca5b7a29bec0bf92c9c25279efedc5e6df70
Umashankar Subramanian <umashankar.s@live.com>
2016-04-03 21:53:41 -0700

    update feature: repond with graphics

--- sample.js
+++ sample.js
@@ -10,6 +10,7 @@ var config = {
     'botCommand': {
       'log': {
         'commandType': 'DATA',
+        'allowedUsers': ['john'],
         'allowedParam': [1, 2],
         'defaultParamValue': 1,
         'template': function() {
@@ -19,8 +20,34 @@ var config = {
           callback({
             'param': param
           });
+        }
+      },
+      'trend': {
+        'commandType': 'DATA',
+        'responseType': {
+          type: 'svg',
+          ylabel: 'errors',
+          timeUnit: 'm',
+          title: 'Log data',
+          logscale: false,
+          style: 'lines'
         },
-        'allowedUsers': ['john']
+        'allowedParam': [1, 2],
+        'defaultParamValue': 1,
+        'data': function(command, param, callback) {
+          var dataArr = [ // Sample data
+            [100, 120, 130, 110, 123, 90],
+            [1, 120, 130, 110, 90, 85],
+            [1, 120, 130, 1010, 140, 145],
+            [100, 120, 130, 250, 140, 145],
+            [100, 120, 130, 300, 140, 145],
+            [100, 400, 130, 300, 140, 145],
+            [100, 90, 130, 300, 140, 145],
+            [100, 120, 130, 1010, 150, 90]
+          ];
+          var rand = dataArr[Math.floor(Math.random() * dataArr.length)];
+          callback(rand);
+        }
       },
       'error': {
         'commandType': 'RECURSIVE',
